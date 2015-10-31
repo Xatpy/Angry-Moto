b913101b990acd4000741270dcab124880f4bcc1
Xatpy <jaime.chapinal@gmail.com>
2015-10-31 12:27:28 +0100

    Adding back buttons

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour {
+
+    public string level = "Main";
+    void OnMouseDown()
+    {
+        Debug.Log("vamos");
+        //Camera.main.GetComponent<AudioSource>().Stop();
+        //GetComponent<AudioSource>().Play();    void OnMouseDown()
+        {
+            //Camera.main.GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().Play();
+            Invoke("CargarNivelJuego", GetComponent<AudioSource>().clip.length);                
+            //Invoke("CargarNivelJuego", GetComponent<AudioSource>().clip.length);
+            //Invoke("LoadMain", 0.5f);
+        }
+    }
+
+    void LoadMain()
+    {
+        Application.LoadLevel(level);
+    }
+}
